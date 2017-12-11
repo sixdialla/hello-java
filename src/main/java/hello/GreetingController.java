feb5d6210937cac7fd65f4e3537a8bca3e9ef508
sixdialla <sidiazll_92@hotmail.com>
2017-12-11 13:43:24 +0100

    Añadiendo GreetingTranslator

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -14,5 +14,12 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "greeting";
     }
+	@RequestMapping("/greeting")
+   	 public String greeting(@RequestParam(value="name", required=false, defaultValue="World") 	String name, @RequestParam(value="lang", required=false, defaultValue="en") String lang, 
+  	Model model) {
+    	model.addAttribute("hello", greetingTranslator.sayHelloIn(lang));
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 
 }
